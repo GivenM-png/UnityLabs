b8cece8c469668009c36349e3a19e665f2c51282
LpMashinini <lebogangp84@gmail.com>
2026-03-25 23:20:00 +0200

    added input

--- UnityLab-Project/src/Components/Form/Signup.jsx
+++ UnityLab-Project/src/Components/Form/Signup.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>
-      sign up here
+    <div className='min-w-90 h-102 bg-white rounded-2xl'>
+      <div>
+        <label>Name</label>
+        <input type="text" placeholder='Enter name' />
+      </div>
     </div>
   )
 }
