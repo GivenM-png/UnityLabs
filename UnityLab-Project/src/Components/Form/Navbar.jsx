d887fbd643638bb264bcdefb9eb3d207987c0857
LpMashinini <lebogangp84@gmail.com>
2026-03-27 00:03:48 +0200

    new Project structure

---
+++ UnityLab-Project/src/Components/Form/Navbar.jsx
@@ -0,0 +1,13 @@
+
+const Navbar = () => {
+  return (
+
+    <div className="w-full h-15 bg-amber-50 p-5">
+      <div>
+        <h1>SKHUL</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
